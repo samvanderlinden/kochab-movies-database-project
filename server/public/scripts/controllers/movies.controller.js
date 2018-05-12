44af865b96787b9e0897fdcd2b49c9b483f81008
Sam Vanderlinden <samvanderlinden6@gmail.com>
2018-05-12 17:09:43 -0500

    append movie posters with the api

--- server/public/scripts/controllers/movies.controller.js
+++ server/public/scripts/controllers/movies.controller.js
@@ -3,14 +3,20 @@ app.controller('MovieController', ['MovieService', 'GenreService', '$http', func
     var self = this;
     self.message = 'Movie Controller';
     self.addMovie = MovieService.addMovie;
-    // self.displayMovies = MovieService.movieList;
     self.getMovies = MovieService.getMovies;
     self.movieList = MovieService.movieList;
     self.deleteMovie = MovieService.deleteMovie;
     self.genreList = GenreService.genreList;
     self.getGenre = GenreService.getGenre;
+    self.newMovie = MovieService.newMovie;
+    self.moviePoster = MovieService.movieList.list;
+    self.getPoster = MovieService.getPoster;
+    
+    // self.getPoster();
     self.getGenre();
     self.getMovies();
+
+   
 
 
 }])
